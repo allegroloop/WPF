81fcb0fab48ac7230060fe05fa438fd11bce9e15
allegroloop <allegroloop@hotmail.com>
2013-04-17 13:10:02 -0700

    Added Arithmetic Operations

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,4 +1,16 @@
+//expressions setup
 var a = 2;
 console.log(a);
 b = a + 3;
 console.log(b)
+//Expressions - Age Example
+var yearBorn = 1981;
+var age = 2013 -yearBorn;
+console.log(age);
+//Expressions - Arithmetic Operators
+//area = 1/2w * h
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area);
+
