f49bc4fed6b81be2b923264ab5bebb4741f62cd7
allegroloop <allegroloop@hotmail.com>
2013-04-17 13:56:10 -0700

    Expressions with Arrays

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -26,4 +26,11 @@ var q4 = 80;
 var avg = (q1 + q2 + q3 + q4)/4;
 console.log(avg);
 
+//Expressions - Expressions with arrays
+var orangeBins = [234,567,883];
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
+var average = total/3;
+console.log(average);
+
 
