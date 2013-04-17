b075d01ed8896f98cd483f9b2e0127e31b27c075
allegroloop <allegroloop@hotmail.com>
2013-04-17 11:45:35 -0700

    Boolean and Variables

--- AssociatingJS2/JS/script.js
+++ AssociatingJS2/JS/script.js
@@ -5,6 +5,14 @@ console.log(yearBorn)
 
 alert("Hello World")
 
-var age = 32
+var age = 32;
 
 var yearOfBirth = 1981;
+
+var isStudent = false;
+
+var myFriend = "Kermit";
+
+var firstName = "Jordan";
+
+var phrase = 'I don\'t know, \n you\'ll have to ask the professor'
