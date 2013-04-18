fbf6d6e6744e57298868a6c15448569d5870870f
allegroloop <allegroloop@hotmail.com>
2013-04-17 21:32:52 -0700

    redefined weeks and days and added the average

--- Industry/js/script.js
+++ Industry/js/script.js
@@ -1,9 +1,9 @@
 // Assignment Expressions - Industry
 // Calculate the average pay per day in a given month
 
-var month = 4; // I used 22 as the number of working weeks in a month
-var week = 5 // Defining how many day are in one work week
+var weeks = 4; // I used 22 as the number of working weeks in a month
+var days = 5 // Defining how many day are in one work week
 var payChecks = [738, 875, 650, 754]; // An arbitrary list of weekly paychecks
-var total = payChecks[0] + payChecks[1] + payChecks[2] + payChecks[3]; //add all the paychecks using and array
+var monthlyTotal = payChecks[0] + payChecks[1] + payChecks[2] + payChecks[3]; //add all the paychecks using and array
 
-var average
+var average = monthlyTotal / weeks; //
