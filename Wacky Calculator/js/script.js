b95957cfd845c5456a4e42000c5a15ac9c5da782
allegroloop <allegroloop@hotmail.com>
2013-04-18 12:28:24 -0700

    aded a comment on redfining the color into numbers

--- Wacky Calculator/js/script.js
+++ Wacky Calculator/js/script.js
@@ -3,3 +3,4 @@
 // First I must define the number among all hair types
 
 var hairTypes = [blonde, brown, black, red] //these are the basic given hair types
+// at this point I need to re-define the colors as specific numbers in order to do the math
