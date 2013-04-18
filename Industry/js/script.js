d71821e45ed6657096803098a1b61dd6c42f3415
allegroloop <allegroloop@hotmail.com>
2013-04-17 21:22:52 -0700

    Added an array to add the total of all the weeks paychecks

--- Industry/js/script.js
+++ Industry/js/script.js
@@ -1,5 +1,6 @@
 // Assignment Expressions - Industry
 // Calculate the average pay per day in a given month
 
-var month = 20; // I used 22 as the number of working days in a month
+var month = 4; // I used 22 as the number of working weeks in a month
 var payChecks = [738, 875, 650, 754]; // An arbitrary list of weekly paychecks
+var total = payChecks[0] + payChecks[1] + payChecks[2] + payChecks[3]; //add all the paychecks using and array
