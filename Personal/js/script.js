b8443fb36ce2d97020ec3ec604824ff33e7352a8
allegroloop <allegroloop@hotmail.com>
2013-04-17 22:13:31 -0700

    var = Math for the lbs of trash recycled in a week

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -1,6 +1,7 @@
 //How much of my trash in lbs is Recyclable in one year
 
-var weeklyTotal = 24 //This is the total amount of trash in lbs
-var weeklyDumpster = 5.8 //This is how much trash actually wen into the dumpster
+var weeklyTotal = 24; //This is the total amount of trash in lbs
+var weeklyDumpster = 5.8; //This is how much trash actually wen into the dumpster
+var weeklyRecycle = weeklyTotal - weeklyDumpster; //Math for the lbs of trash recycled in a week
 
 
