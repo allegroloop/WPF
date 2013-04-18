0a54a428dae8114ea059a907988d40bf047a7ad4
allegroloop <allegroloop@hotmail.com>
2013-04-17 21:27:53 -0700

    Inserted on line 5 to define how many days are in a work week

--- Industry/js/script.js
+++ Industry/js/script.js
@@ -2,5 +2,8 @@
 // Calculate the average pay per day in a given month
 
 var month = 4; // I used 22 as the number of working weeks in a month
+var week = 5 // Defining how many day are in one work week
 var payChecks = [738, 875, 650, 754]; // An arbitrary list of weekly paychecks
 var total = payChecks[0] + payChecks[1] + payChecks[2] + payChecks[3]; //add all the paychecks using and array
+
+var average
