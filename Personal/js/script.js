56d49a18cbf7806acaf9119ecb0764beb9625aa3
allegroloop <allegroloop@hotmail.com>
2013-04-18 14:33:37 -0700

    added name and date to assignment

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -1,4 +1,4 @@
-//How much of my trash in lbs is Recyclable in one year
+//James "Jordan" Rocha_4-17-2013_Personal Calculator "How much of my trash in lbs is Recyclable in one year"
 
 var weeklyTotal = 24; //This is the total amount of trash in lbs
 var weeklyDumpster = 5.8; //This is how much trash actually wen into the dumpster
