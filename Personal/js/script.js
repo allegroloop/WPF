95dffeec7e2ca6c9fb5a27b9ae7510262ce039cc
allegroloop <allegroloop@hotmail.com>
2013-04-17 22:22:46 -0700

    PEMDAS - Calculate how many weeks are in a year by dividing the number of day in a year by the number of days in a week and then multiply that by the weekly total of lbs of recycled material.

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -4,4 +4,6 @@ var weeklyTotal = 24; //This is the total amount of trash in lbs
 var weeklyDumpster = 5.8; //This is how much trash actually wen into the dumpster
 var weeklyRecycle = weeklyTotal - weeklyDumpster; //Math for the lbs of trash recycled in a week
 
+var yearlyRecycle = 365.25 / 7 * weeklyRecycle; // PEMDAS - Calculate how many weeks are in a year by dividing the number of day in a year by the number of days in a week and then multiply that by the weekly total of lbs of recycled material.
 
+console.log =
