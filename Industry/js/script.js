3cdef8c12610ee7db7450f5f7eefac88effe2b2a
allegroloop <allegroloop@hotmail.com>
2013-04-17 20:53:07 -0700

    Modified Month total to 20 and paychecks to 4 weeks

--- Industry/js/script.js
+++ Industry/js/script.js
@@ -1,5 +1,5 @@
 // Assignment Expressions - Industry
 // Calculate the average pay per day in a given month
 
-var month = 22; // I used 22 as the number of working days in a month
-var payChecks = [738, 875, 650, 754, 374]; // An arbitrary list of weekly paychecks
+var month = 20; // I used 22 as the number of working days in a month
+var payChecks = [738, 875, 650, 754]; // An arbitrary list of weekly paychecks
