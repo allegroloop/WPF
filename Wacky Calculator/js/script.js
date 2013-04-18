0c39a8d6c17257ecda707715d573d9c51dc2805f
allegroloop <allegroloop@hotmail.com>
2013-04-18 13:07:42 -0700

    Noticed error on console log. Added missing semicolon to each line of code.

--- Wacky Calculator/js/script.js
+++ Wacky Calculator/js/script.js
@@ -2,16 +2,16 @@
 
 // First I must define the number among all hair types
 
-var hairTypes = [blonde, brown, black, red] //these are the basic given hair types
+var hairTypes = [blonde, brown, black, red]; //these are the basic given hair types
 
 // at this point I need to re-define the colors as specific numbers of actual hairs on head in order to do the math
 
-hairTypes[0] = 140000 //blonde Consensus found on http://htg.addr.com/didyou.htm
-hairTypes[1] = 110000 // brown Consensus found on http://htg.addr.com/didyou.htm
-hairTypes[2] = 108000 // black Consensus found on http://htg.addr.com/didyou.htm
-hairTypes[3] = 90000 //red Consensus found on http://htg.addr.com/didyou.htm
+hairTypes[0] = 140000; //blonde Consensus found on http://htg.addr.com/didyou.htm
+hairTypes[1] = 110000; // brown Consensus found on http://htg.addr.com/didyou.htm
+hairTypes[2] = 108000; // black Consensus found on http://htg.addr.com/didyou.htm
+hairTypes[3] = 90000; //red Consensus found on http://htg.addr.com/didyou.htm
 
-var age = 32 // My Age, shh, don't tell
+var age = 32; // My Age, shh, don't tell
 
-console.log(hairTypes[3] + " " + "hairs")
+console.log(hairTypes[3] + " " + "hairs");
 
