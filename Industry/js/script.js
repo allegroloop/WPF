d70dd3090693ee8f680b9fc61baec6db32b61f06
allegroloop <allegroloop@hotmail.com>
2013-04-17 20:51:18 -0700

    Array of weekly paychecks

--- Industry/js/script.js
+++ Industry/js/script.js
@@ -2,4 +2,4 @@
 // Calculate the average pay per day in a given month
 
 var month = 22; // I used 22 as the number of working days in a month
-var payChecks = [738, 875, 650, 754, 374]; //
+var payChecks = [738, 875, 650, 754, 374]; // An arbitrary list of weekly paychecks
