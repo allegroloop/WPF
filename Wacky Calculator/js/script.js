9ec15800c441da75fef46e565e730906f319ed8d
allegroloop <allegroloop@hotmail.com>
2013-04-18 13:03:10 -0700

    spaced out and reorganized

--- Wacky Calculator/js/script.js
+++ Wacky Calculator/js/script.js
@@ -3,7 +3,9 @@
 // First I must define the number among all hair types
 
 var hairTypes = [blonde, brown, black, red] //these are the basic given hair types
+
 // at this point I need to re-define the colors as specific numbers of actual hairs on head in order to do the math
+
 hairTypes[0] = 140000 //blonde Consensus found on http://htg.addr.com/didyou.htm
 hairTypes[1] = 110000 // brown Consensus found on http://htg.addr.com/didyou.htm
 hairTypes[2] = 108000 // black Consensus found on http://htg.addr.com/didyou.htm
