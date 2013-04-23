3c71ee8af9f794c2468d18283b7bbdf6580c2f0c
allegroloop <allegroloop@hotmail.com>
2013-04-22 19:23:41 -0700

    Added "Age" Prompt to Expressions_Wacky Calculator

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,16 +3,20 @@
 
 // First I must define the number among all hair types
 
+
 var hairTypes = ["blonde", "brown", "black", "red"]; //these are the basic given hair types
 
 // at this point I need to re-define the colors as specific numbers of actual hairs on head in order to do the math
 
-hairTypes[0] = 140000; //blonde Consensus found on http://htg.addr.com/didyou.htm
+hairTypes[0] = 140000; // blonde Consensus found on http://htg.addr.com/didyou.htm
 hairTypes[1] = 110000; // brown Consensus found on http://htg.addr.com/didyou.htm
 hairTypes[2] = 108000; // black Consensus found on http://htg.addr.com/didyou.htm
-hairTypes[3] = 90000; //red Consensus found on http://htg.addr.com/didyou.htm
+hairTypes[3] = 90000; // red Consensus found on http://htg.addr.com/didyou.htm
 
-var age = 32; // My Age, shh, don't tell
+//I dont know how to prompt a choice yet, so I am going to define my hair color as brown.
+
+
+var age = prompt ("What is your age?"); // Prompt to define user's name
 
 console.log(hairTypes[1] + " " + "hairs"); //ran a test, noticed i needed semicolons at the end of each line and i was missing quotations in my array. I got test to work.
 
