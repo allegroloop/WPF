0a63feeb793a4c6bf83a3cf71aa41ab8a5442857
allegroloop <allegroloop@hotmail.com>
2013-04-23 19:28:26 -0700

    Wrote out code

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -1,7 +1,30 @@
-/**
- * Created with JetBrains WebStorm.
- * User: JROC
- * Date: 4/23/13
- * Time: 6:40 PM
- * To change this template use File | Settings | File Templates.
- */
+// Jordan Rocha_4/22/13_Conditionals Worksheet
+
+//Get Gas?
+
+//Determine if driver has enough fuel.
+
+//If car has enough fuel, drive on
+
+//if car cannot make it 200 mile, stop for gas.
+
+
+var mpg = 20;
+var gasPercent = 50;
+var gasCapacity = 20;
+
+var fuel = gasCapacity / gasPercent * mpg;
+var fuelRemaining = gasCapacity / gasPercent;
+
+if(fuel >= 200){
+
+    console.log("Yes,you can make it without stopping for gas!");
+}
+
+if(fuel < 200){
+
+    console.log("You only have" + " " fuelRemaining + " " + "gallons of gas in your gas tank, better get gas while you can")
+}
+
+
+
