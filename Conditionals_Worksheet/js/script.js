c13e1289a8fbe7604d40a387829abc68c2885307
allegroloop <allegroloop@hotmail.com>
2013-04-23 20:32:42 -0700

    Commented on code

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -13,17 +13,16 @@ var mpg = 20;
 var gasPercent = .5;
 var gasCapacity = 20;
 
-var fuel = gasCapacity * gasPercent * mpg;
-var fuelRemaining = gasCapacity * gasPercent;
+var fuel = gasCapacity * gasPercent * mpg; //gas capacity * the 50%(written as.5) * times the miles per gallon gives me my fuel in how many miles I can drive
+var fuelRemaining = gasCapacity * gasPercent;//how much gas is remaining. 50% of the capacity
 
-console.log(fuel);
 
 if(fuel >= 200){
     //if fuel is greater than or equal to then drive on
-    console.log("Yes,you can make it without stopping for gas/!");
+    console.log("Yes,you can make it without stopping for gas!");
 }else{
     //if fuel is less than 200, then stop and get gas
-    console.log("You only have" + " " fuelRemaining + " " + "gallons of gas in your gas tank, better get gas while you can!")
+    console.log("You only have" + " " fuelRemaining + " " + "gallons of gas in your gas tank, better get gas while you can!");
 }
 
 
