f63fb80a12f30ffffff3df9b1257a840a56ee8a1
allegroloop <allegroloop@hotmail.com>
2013-04-23 21:52:36 -0700

    Grade Calculator. First comment, prompt for grade and first line of code.

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -10,7 +10,7 @@
 
 
 var mpg = 20;
-var gasPercent = .1;
+var gasPercent = .1;  //Amount in the gas tank
 var gasCapacity = 20;
 
 var fuel = gasCapacity * gasPercent * mpg; //gas capacity * the 50%(written as.5) * times the miles per gallon gives me my fuel in how many miles I can drive
@@ -23,6 +23,18 @@ if(fuel >= 200){
 }else{
     //if fuel is less than 200, then stop and get gas
     console.log("You only have" + " " + fuelRemaining + " " + "gallons of gas in your gas tank, better get gas while you can!");
+}
+
+
+//Grade Letter Calculator
+
+//Student earns a number grade. Determine the letter grade using conditional statements.
+
+var grade = prompt ("What is you grade for this course",95); //prompt for users grade
+
+if(grade <= 100 && grade >= 90){
+    //if grade is greater than 89 and equal or less to 100 than write out an A
+    console.log("Great Job! You got an A.")
 }
 
 
