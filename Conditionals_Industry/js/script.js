2920feab1f56ff6c8d07bdbcb959cef896a8a6a5
allegroloop <allegroloop@hotmail.com>
2013-04-25 15:11:51 -0700

    commented on var's

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,5 +4,5 @@
 
 //If the Student is on time he gets full credit. If student is 5-15 min late than he gets a tardy. If student is more than 30 min late then he gets no credit for class.
 
-var arrivalTime = prompt("How many minutes after the scheduled class start time did student arrive?");
-var excuse = confirm("Does student have an excuse?");
+var arrivalTime = prompt("How many minutes after the scheduled class start time did student arrive?"); //define var time for student
+var excuse = confirm("Does student have an excuse?"); //define var excuse true/false
