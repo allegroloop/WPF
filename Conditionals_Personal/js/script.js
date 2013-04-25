c63b5609588787d01973daf0116506b024dc6a02
allegroloop <allegroloop@hotmail.com>
2013-04-25 13:33:06 -0700

    If else code for raining and day off added

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,7 +4,19 @@
 
 //Conditions: 1.Do you have the day off, yes or no? , 2.Is it sunny or raining?
 
-var dayOff = prompt("Do you have the day off, yes or no?");
-var weather = prompt("Is it sunny or raining");
+var dayOff = prompt("Do you have the day off, yes or no?"); //prompt question to define day off as yes or no
+var weather = prompt("Is it sunny or raining"); //prompt question to define weather as raining or sunny
 
-console.log(dayOff + " " + weather);
+console.log(dayOff + " " + weather); //var prompts checked as working
+
+if("yes" && "sunny"){
+    //if prompts are answered as yes and sunny - take a hike buddy!
+    console.log("Take a hike buddy!");
+}else if("yes" && "raining"){
+    //if answers are yes and raining - print to console "Not today, work on Java Script instead."
+    console.log("Not today, work on Java Script instead.");
+}else{
+    //everything else = "Nice try, go to work!"
+    console.log("Nice try! Go to work.");
+
+}
