ba3bc6bc81c016fd718051bb7be8d25f5af1137e
allegroloop <allegroloop@hotmail.com>
2013-04-25 19:19:45 -0700

    If its between 8 and 12 and you have eaten than drink some coffee

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -7,3 +7,12 @@
 //if it's between 0800 and 1200 and you have eaten in the last hour, then drink coffee.
 //if it's between 1200 and 1900 and you have eaten in the last hour, than drink tea.
 //all else drink water.
+
+var time = prompt("What time is it in military time?"); //define prompt as the time of day it is
+var chowTime = confirm ("Have you eaten in the last hour, ok for yes or cancel for no"); //define prompt as true or false
+
+
+if(time >= 0800 && time <=1200 && chowTime == true){
+    // If its between 8 and 12 and you have eaten in the last hour then you can drink some coffee
+     console.log("It's java time!")
+}
