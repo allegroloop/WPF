fda2862c798f035c91790e71e39eefdfac1be9dd
allegroloop <allegroloop@hotmail.com>
2013-04-25 18:38:48 -0700

    Define condition and solution as What beverage should you drink depending on the time of day and whether or not you have eaten.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,9 @@
 //Jordan Rocha_4/23/13_Conditional Wacky
+
+//What beverage should I drink?
+
+//Depending on what time of the day it is and when the last time I have eaten, decide whether to drink coffee, tea, or water.
+
+//if it's between 0800 and 1200 and you have eaten in the last hour, then drink coffee.
+//if it's between 1200 and 1900 and you have eaten in the last hour, than drink tea.
+//all else drink water.
