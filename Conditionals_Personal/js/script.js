7e5a41ca9dad9a439dbbfa898e843930d697bbbd
allegroloop <allegroloop@hotmail.com>
2013-04-25 19:43:34 -0700

    checked code by printing to console. Code confirmed as working

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -14,6 +14,6 @@ console.log(dayOff + " " + weather); //var prompts checked as working
      //rewrite working code as Ternary
 
 (dayOff >= 1 && dayOff <=5 && weather > 54 && weather < 81) ? console.log("Take a hike buddy!") : console.log("Not today, work on Java Script instead.");
-    //if prompts are answered as yes and sunny - take a hike buddy!
+    //if prompts are answered as time on or between 1 and 5 and the temperature is between 55 and 80 degrees then print to console "take a hike buddy!"
     //if answers anything else - print to console "Not today, work on Java Script instead."
 
