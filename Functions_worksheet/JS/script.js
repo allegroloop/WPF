0eedf323fa6071158115ef16227951f0907c9628
allegroloop <allegroloop@hotmail.com>
2013-04-30 13:55:38 -0700

    Var prompt for "weight"

--- Functions_worksheet/JS/script.js
+++ Functions_worksheet/JS/script.js
@@ -3,4 +3,6 @@
 //It takes 8.666666667 bee stings per pound to kill an animal.
 //Calculate how many bee stings are needed to kill an animal in a function.
 
+var weight = prompt("What is the weight of the animal?")//prompt for the variable "weight" for calculations.
+
 
