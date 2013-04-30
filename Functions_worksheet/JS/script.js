6abd020044f4bcbf0460a00c9b00cf4f39056019
allegroloop <allegroloop@hotmail.com>
2013-04-30 13:50:00 -0700

    Stated the problem. How many bee stings to kill an animal by lb?

--- Functions_worksheet/JS/script.js
+++ Functions_worksheet/JS/script.js
@@ -1,7 +1,6 @@
-/**
- * Created with JetBrains WebStorm.
- * User: JROC
- * Date: 4/30/13
- * Time: 11:12 AM
- * To change this template use File | Settings | File Templates.
- */
+//Jordan Rocha_4/30/13_Functions Worksheet
+
+//It takes 8.666666667 bee stings per pound to kill an animal.
+//Calculate how many bee stings are needed to kill an animal in a function.
+
+
