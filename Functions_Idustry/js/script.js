fdaa11225d06a2dd2d6060635d16536f35aaab3b
allegroloop <allegroloop@hotmail.com>
2013-05-01 18:13:20 -0700

    wrote var prompt and percent function

--- Functions_Idustry/js/script.js
+++ Functions_Idustry/js/script.js
@@ -6,3 +6,10 @@
 //If 74-35% attending, then print "We need to work on getting our numbers up.";
 //If below 35%, then print "Give up, this class is a bust.";
 
+var students = prompt("How many students showed up to class today?");
+
+function percent = (students){
+    var classSize = 16
+    var percent = students/ classSize
+}
+
