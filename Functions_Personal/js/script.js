5cbcbf0d5300eb6cda872ff8457991bf06e4897b
allegroloop <allegroloop@hotmail.com>
2013-05-02 16:32:52 -0700

    Added comments and checked code. Tested as working for the volume function.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,17 +6,15 @@
 //What is the volume of the sink in inches? Length, width , height?
 //Assume the average volume of the dish is 40 inches cubed and prompt how many dishes are dirty?
 
-var length = prompt("What is the length of your sink in inches?");
-var width = prompt("What is the length of your sink in inches?");
-var height = prompt("What is the length of your sink in inches?");
+var length = prompt("What is the length of your sink in inches?");//prompt for length
+var width = prompt("What is the width of your sink in inches?");//prompt for width
+var height = prompt("What is the height of your sink in inches?");//prompt for height
 
-var sinkVolume = calcVolume(length,width,height);
+var sinkVolume = calcVolume(length,width,height); //store return function in the var sinkVolume
 
-function calcVolume(l, w, h){
+function calcVolume(l, w, h){  //function for volume is length time width time height
     var volume = l * w * h;
-    return volume;
+    return volume;  //return command volume to above sinkVolume var
 }
 
-
-
-console.log(sinkVolume);
+console.log(sinkVolume); //sink volume function tested as working.
