0a53f69fb613ff2355bbe8103e316fc15952bb00
allegroloop <allegroloop@hotmail.com>
2013-05-01 19:58:42 -0700

    commented on lines of code and tested code. Tested as working

--- Functions_Idustry/js/script.js
+++ Functions_Idustry/js/script.js
@@ -9,19 +9,19 @@
 var students = prompt("How many students showed up to class today?");//prompt to define number of students present in class
 var percent = calcPercent(students);//return to store calcPercent of function
 
-function calcPercent(students){
-    var classSize = 16;
-    var classPercent = students/ classSize * 100;
-    return classPercent;
+function calcPercent(students){ //function to send "students" prompt to classPercent equation
+    var classSize = 16;//standard class size
+    var classPercent = students/ classSize * 100;//percentage equation = number of students divided
+    return classPercent;//return command to send function result to be stored in var percent
 }
-calcPercent(students);
+calcPercent(students);// i believe this send the prompt "students" to the function calcPercent's perameter
 
-if(percent >= 75){
+if(percent >= 75){  //If 75% or more attendance, then print
     console.log (percent + "% of your class showed up today. Your class is a success!");
-}else if(percent <=74 && percent > 34){
+}else if(percent <=74 && percent > 34){  //If between 74-35% attending, then print "We need to work on getting our numbers up.";
     console.log(percent + "% of your class showed up today. Need to work on getting people to show up!");
-}else{
+}else{ //If below 35%, then print "Give up, this class is a bust.";
     console.log(percent + "% of your class showed up today. This class is a bust. Just give up!");
 
-}
+}        //code tested as working
 
