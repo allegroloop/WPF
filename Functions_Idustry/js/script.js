17560c9941482f73309380c9c6f16cef58ddad6a
allegroloop <allegroloop@hotmail.com>
2013-05-01 18:02:53 -0700

    FIrst commit. Commented on and stated approach…Is this class meeting attendance requrments?

---
+++ Functions_Idustry/js/script.js
@@ -0,0 +1,8 @@
+//Jordan Rocha-5_1_13_Functions: Industry
+
+//Is this class meeting attendance requirements?
+
+//If 75% or more attendance, then print "The class is a success.";
+//If 74-35% attending, then print "We need to work on getting our numbers up.";
+//If below 35%, then print "Give up, this class is a bust.";
+
