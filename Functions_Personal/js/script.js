afcb5cbdd82375f0013b1782b0f91aeaac18c3bd
allegroloop <allegroloop@hotmail.com>
2013-05-02 16:17:19 -0700

    Mispelled "script" on index causing console print errors. Fixed return code sinkVolume to hold function peramerters.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,7 +1,22 @@
 //Jordan Rocha_5/2/13_Functions: Personal
 
-//How long until Ineed to do the dishes?
+//How long until I need to do the dishes?
 
 
 //What is the volume of the sink in inches? Length, width , height?
 //Assume the average volume of the dish is 40 inches cubed and prompt how many dishes are dirty?
+
+var length = prompt("What is the length of your sink in inches?");
+var width = prompt("What is the length of your sink in inches?");
+var height = prompt("What is the length of your sink in inches?");
+
+var sinkVolume = calcVolume(length,width,height);
+
+function calcVolume(l, w, h){
+    var volume = l * w * h;
+    return volume;
+}
+
+
+
+console.log(sinkVolume);
