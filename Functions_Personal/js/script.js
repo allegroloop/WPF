dc71e94b585b62189192b6e650fd3cc60386b548
allegroloop <allegroloop@hotmail.com>
2013-05-02 15:47:09 -0700

    first commit. State question in comments. Do I need to do th dishes?

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,7 @@
+//Jordan Rocha_5/2/13_Functions: Personal
+
+//How long until Ineed to do the dishes?
+
+
+//What is the volume of the sink in inches? Length, width , height?
+//Assume the average volume of the dish is 40 inches cubed and prompt how many dishes are dirty?
