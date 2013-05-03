a64b37cfb88461c0bf036c83f87cad113efce494
allegroloop <allegroloop@hotmail.com>
2013-05-02 18:14:43 -0700

    Added and refined comments and tested code.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,19 +6,21 @@
 
 //How many hours will you be looking at the sky?
 
-var gazeTime = prompt("How many hours will you be looking at the sky?");
+var gazeTime = prompt("How many hours will you be looking at the sky?"); //prompt four hour star gazing
 
 
 
-var starChance = function(gazeTime){
-    var visibility = .00005 //amount of sky visible tou where we stand in the world
+var starChance = function(gazeTime){//my attempt at an anonymous function
+    var visibility = .00005 //amount of sky visible to you where you stand in the world
     var day = 24 //hours in the day
     var meteorites = 1000000 // approx number of meteorites in a 24 hour period
-    var chance =  meteorites * visibility / day * gazeTime
-    return chance;
+    var chance =  meteorites * visibility / day * gazeTime //PEMDAS - ordered numbers to get the correct calculations
+    return chance;// return "chance" to function answer
 
 }
 
-var shootingStars = starChance(gazeTime);
+var shootingStars = starChance(gazeTime);//defined shooting stars and invoked the function
 
 console.log("If you are outside at night and look at the sky for " + gazeTime + " hours, then you will probably see " + shootingStars + " shooting stars.");
+
+//code tested as working.
