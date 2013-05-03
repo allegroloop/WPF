0fb6ccc2f966876d48de956c1fbaed177f02770c
allegroloop <allegroloop@hotmail.com>
2013-05-02 18:08:56 -0700

    attempted anonymous function.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,4 +2,23 @@
 
 //When you wish upon a star, will your dreams come true?
 
+//calculate the odds of seeing a shooting star
 
+//How many hours will you be looking at the sky?
+
+var gazeTime = prompt("How many hours will you be looking at the sky?");
+
+
+
+var starChance = function(gazeTime){
+    var visibility = .00005 //amount of sky visible tou where we stand in the world
+    var day = 24 //hours in the day
+    var meteorites = 1000000 // approx number of meteorites in a 24 hour period
+    var chance =  meteorites * visibility / day * gazeTime
+    return chance;
+
+}
+
+var shootingStars = starChance(gazeTime);
+
+console.log("If you are outside at night and look at the sky for " + gazeTime + " hours, then you will probably see " + shootingStars + " shooting stars.");
